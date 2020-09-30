f7fefa7f9cb0af58af432f63920d1ef9c3c26eef
MSC-Brian <brian.salant@mirlogic.com>
2020-09-30 16:21:21 -0400

    Jobs are scraped and shown

--- src/components/Join_Us/OpeningsList.jsx
+++ src/components/Join_Us/OpeningsList.jsx
@@ -1,21 +1,88 @@
 import React, { Component, useState, useEffect } from 'react'
 import CreateOpeningForm from './CreateOpeningForm';
 import JobOpen from './JobOpen';
+import uuid from 'react-uuid';
 
-const OpeningsList = () => {
-    const [currentTime, setCurrentTime] = useState(0);
+class OpeningsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            openings: [],
+            listing: [],
+            link: [],
+            place1: [],
+            type1: []
+         }
+    }
 
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/time').then(res => res.json()).then(data => {setCurrentTime(data.time);});
-        fetch('http://127.0.0.1:5000/job_postings').then(res => res.json()).then(data => {console.log(data);});}, []);
+    fun1 = () => {
+        console.log(this.state.openings);
+    }
 
-    console.log(currentTime)
-    return(
-        <div>
-            <p><strong>The current time is {currentTime}.</strong></p>
-        </div>
-    )
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/job_postings').then(res => res.json()).then(data => {
+            console.log(data);
+            this.setState({
+                openings: Object.values(data)[0],
+                // listing: Object.values(data)[0][0].LISTING,
+                // link: Object.values(data)[0][0].LINK,
+                // place1: Object.values(data)[0][0].PLACE_1,
+                // type1: Object.values(data)[0][0].TYPE_1
+            })
+            this.fun1();
+        })
+    }
+        
+    render() { 
+        const openings = this.state.openings.map(opening => {
+            return ( 
+                <JobOpen key={uuid()}
+                            title={opening.LISTING}
+                            location={opening.PLACE_1}
+                            type={opening.TYPE_1}
+                            link={opening.LINK}
+                            location2={opening.PLACE_2}
+                            type2={opening.TYPE_2}
+                    />
+            );
+        });
+        return ( 
+            <div>
+                {Array.isArray(this.state.openings) && this.state.openings.length === 0 ? "NO OPENINGS AT THIS TIME" : <span>{openings}</span>}
+                <br />
+                <CreateOpeningForm createOpening={this.createNewOpening} />
+            </div>
+         );
+    }
 }
+
+// const OpeningsList = () => {
+
+//     const [openings, setOpenings] = useState(0);
+
+//     // const [currentTime, setCurrentTime] = useState(0);
+
+//     useEffect(() => {
+//         // fetch('http://127.0.0.1:5000/time').then(res => res.json()).then(data => {setCurrentTime(data.time);});
+
+//         fetch('http://127.0.0.1:5000/job_postings').then(res => res.json()).then(data => {
+//             console.log(data);
+//             setOpenings(Object.values(data));
+//         });}, []);
+
+//     // console.log(currentTime)
+
+//        console.log(openings);
+//     return(
+//         <div>
+//             <p><strong>The current time is Beer Thirty.</strong></p>
+//         </div>
+//     )
+// }
+
+// OpeningsList.defaultProps = {
+
+// }
 
 // class OpeningsList extends Component {
 //     constructor(props) {
